870905938346a93bcd27c7777dd3f456daa99973
EviLuci <sujan.koju111@gmail.com>
2024-10-15 21:26:52 +0545

    added dependencies

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -3,6 +3,16 @@
 // Generated file. Do not edit.
 //
 
+// @dart = 2.13
 // ignore_for_file: type=lint
 
-void registerPlugins() {}
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_messaging_web/firebase_messaging_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebaseMessagingWeb.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
