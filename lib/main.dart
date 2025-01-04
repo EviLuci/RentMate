03bdc817e81d01de5ba7f280b65577b38a10380e
EviLuci <sujan.koju111@gmail.com>
2025-01-04 20:45:49 +0545

    added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,11 @@ import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -14,21 +17,27 @@ void main() async {
   runApp(const RentMateApp());
 }
 
-class RentMateApp extends StatelessWidget {
+class RentMateApp extends StatefulWidget {
   const RentMateApp({super.key});
 
   @override
+  State<RentMateApp> createState() => _RentMateAppState();
+}
+
+class _RentMateAppState extends State<RentMateApp> {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'RentMate',
-        theme: AppTheme.lightTheme,
-        darkTheme: AppTheme.darkTheme,
-        themeMode: ThemeMode.system,
-        initialRoute: AppRoutes.splash,
-        onGenerateRoute: AppRoutes.generateRoute,
-        home: Scaffold(
-          appBar: AppBar(title: Text('RentMate')),
-          body: Center(child: Text('Welcome to RentMate')),
-        ));
+      title: 'RentMate',
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
+      initialRoute: AppRoutes.splash,
+      onGenerateRoute: AppRoutes.generateRoute,
+      home: Scaffold(
+        appBar: AppBar(title: Text('RentMate')),
+        body: Center(child: Text('Welcome to RentMate')),
+      ),
+    );
   }
 }
